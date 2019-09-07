89c0042df2502b2a41bbeefd2425c403d21f007b
mkirc <m.p.kirchner@gmx.de>
2019-09-07 20:49:19 +0200

    added searchByDate functionality

--- public/by_date.php
+++ public/by_date.php
@@ -24,25 +24,80 @@ mysqli_close($con);
 
 
 
-
 <center>
     <table class="table table-bordered table-condensed">
         <tbody>
         	<tr>
         		<td>
-					<select name="date">
+					<select name="date" form="dateform">
 					  <option selected="selected">Choose date</option>
 					  <?php
 					    foreach($rows as $date) { ?>
-					      <option value="<?php echo date('M j Y',strtotime($date['scrap_ts'])) ?>"><?php echo date('M j Y',strtotime($date['scrap_ts'])) ?></option>
+					      <option value="<?php echo $date['scrap_ts'] ?>"><?php echo date('M j Y',strtotime($date['scrap_ts'])) ?></option>
 					  <?php
 					    } ?>
 					</select>
+        		</td>
+        		<td>
+        			<form action="#" id="dateform" method="post">
+        				<input type="submit" value="SuperCoolButtonYo">
+        			</form>
         		</td>
         	</tr>
         </tbody>
     </table>
 </center>
+
+<!-- <?php
+
+    if(isset($_POST))
+    {
+        echo htmlentities($_POST['date']);
+        print_r($_POST);
+    }
+
+?> -->
+
+<?php
+function getGenByDate($rows) {
+	$outarray = array();
+	foreach ($rows as $a) {
+		// echo $a;
+		// print_r($a['scrap_ts']);
+		if((string)$a['scrap_ts'] === (string)$_POST['date']) {
+			// echo 'yay';
+			array_push($outarray, $a);
+		}
+	}
+	return $outarray;
+}
+
+?>
+
+<!-- <?php
+// echo getGenByDate();
+print_r(getGenByDate($rows));
+?>
+ -->
+<center>
+    <table class="table table-bordered table-condensed">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Text</th>
+            </tr>
+        </thead>
+        <tbody>
+			<?php foreach (getGenByDate($rows) as $input): array_map('htmlentities', $input); ?>
+			    <tr>
+			        <td><?php echo htmlspecialchars($input['gen_id']); ?></td>
+			        <td><?php echo htmlspecialchars($input['gen_txt']); ?></td>
+			    </tr>
+			<?php endforeach; ?>
+        </tbody>
+    </table>
+</center>
+
 
 
 <?php include "templates/footer.php"; ?>
